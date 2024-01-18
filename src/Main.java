34a7788fbb409db038cb7f7008961a35d2bb88a0
AlexDv <DvoinikovAV@yandex.ru>
2024-01-18 09:30:12 +0300

    Доманяя работа 3 задание 1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+
+        byte b=125;
+        short sh=-32000;
+        int i=1258425;
+        long l=40025814583L;
+        float fl=12587125.56845f;
+        double dbl=154.85648724;
+        char chr='В';
+        boolean bln=true;
+        System.out.println("Целочисленные переменные:");
+        System.out.println("Значение переменной b с типом byte равно "+b);
+        System.out.println("Значение переменной sh с типом short равно "+sh);
+        System.out.println("Значение переменной i с типом int равно "+i);
+        System.out.println("Значение переменной l с типом long равно "+l);
+        System.out.println("");
+        System.out.println("Вещественные переменные:");
+        System.out.println("Значение переменной fl с типом float равно "+fl);
+        System.out.println("Значение переменной dbl с типом double равно "+dbl);
+        System.out.println("");
+        System.out.println("Строковые переменные:");
+        System.out.println("Значение переменной chr с типом char равно "+chr);
+        System.out.println("");
+        System.out.println("Логические переменные:");
+        System.out.println("Значение переменной bln с типом boolean равно "+bln);
+
+    }
+}
